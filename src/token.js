29299597751519c588b527cc56988fecbac57483
pcreem <creempepper@gmail.com>
2020-09-08 03:20:22 +0800

    better

--- src/token.js
+++ src/token.js
@@ -3,9 +3,9 @@ const AUTH_TOKEN = process.env.REACT_APP_AUTH_TOKEN;
 const USER_NAME = process.env.REACT_APP_USER_NAME;
 
 export const getToken = () => Cookies.get(AUTH_TOKEN);
-export const setToken = token => Cookies.set(AUTH_TOKEN, token, { expires: 7 });
+export const setToken = token => Cookies.set(AUTH_TOKEN, token, { sameSite: 'strict', secure: true, expires: 1 });
 export const deleteToken = () => Cookies.remove(AUTH_TOKEN);
 
 export const getUserName = () => Cookies.get(USER_NAME);
-export const setUserName = name => Cookies.set(USER_NAME, name, { expires: 7 });
+export const setUserName = name => Cookies.set(USER_NAME, name, { sameSite: 'strict', secure: true, expires: 1 });
 export const deleteUserName = () => Cookies.remove(USER_NAME);
